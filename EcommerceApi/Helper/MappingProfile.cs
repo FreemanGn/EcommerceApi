4f04b404cab1bcd48c0d7f9feb06c77cfd836a96
medoum <mohamedoum2430@gmail.com>
2023-10-09 21:52:29 +0100

    category crud

---
+++ EcommerceApi/Helper/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AutoMapper;
+using EcommerceApi.Dtos;
+using EcommerceApi.Models;
+
+namespace EcommerceApi.Helper
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Category, CategoryDto>();
+			CreateMap<CategoryDto, Category>();
+		}
+	}
+}
+
--- EcommerceApi/Program.cs
+++ EcommerceApi/Program.cs
@@ -1,11 +1,22 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using EcommerceApi.Data;
+using EcommerceApi.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
