1f79bf0b6c3fdbd9c012f536813f5ef5bc325232
Freeman_Gn <49251879+FreemanGn@users.noreply.github.com>
2023-10-09 22:49:52 +0100

    Crud category (#4)
    * config project
    
    * add entity
    
    * add entity
    
    * global config
    
    * category crud
    
    * reverse map
    
    ---------
    
    Co-authored-by: katakuri27 <amacusnemo9596@gmail.com>
    Co-authored-by: medoum <mohamedoum2430@gmail.com>

---
+++ EcommerceApi/Helper/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+using EcommerceApi.Dtos;
+using EcommerceApi.Models;
+
+namespace EcommerceApi.Helper
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Category, CategoryDto>().ReverseMap();
+		}
+	}
+}
+
--- EcommerceApi/Models/Category.cs
+++ EcommerceApi/Models/Category.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
 namespace EcommerceApi.Models
 {
 	public class Category
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-
+		public string Image { get; set; }
+		public string Description { get; set; }
+		public DateTime CreatedAt { get; set; }
 	}
 }
 
