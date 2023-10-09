f76aa72150ba119a390202d4c71b0274c1ab012e
Freeman_Gn <49251879+FreemanGn@users.noreply.github.com>
2023-10-09 22:35:41 +0100

    Revert "category crud"
    This reverts commit 4f04b404cab1bcd48c0d7f9feb06c77cfd836a96.

--- EcommerceApi/Program.cs
+++ EcommerceApi/Program.cs
@@ -1,22 +1,11 @@
-﻿using EcommerceApi.Data;
-using EcommerceApi.Services;
-using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
 
 var app = builder.Build();
 
