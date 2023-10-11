db5d8df8b1ccced4f9e509410e93299b73ebd966
medoum <mohamedoum2430@gmail.com>
2023-10-11 22:10:00 +0100

    try fix conflic final

--- EcommerceApi/Models/Category.cs
+++ EcommerceApi/Models/Category.cs
@@ -1,14 +1,11 @@
-﻿using System;
+using System;
 namespace EcommerceApi.Models
 {
 	public class Category
 	{
 		public string Name { get; set; }
-
 		public string Image { get; set; }
-
 		public string Description { get; set; }
-
 		public DateTime CreatedAt { get; set; }
 	}
 }
