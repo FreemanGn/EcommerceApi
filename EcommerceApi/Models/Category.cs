5a1ccd58cf267605a31bcb299ef0b47c4569237a
medoum <mohamedoum2430@gmail.com>
2023-10-11 22:08:04 +0100

    try fix conflic two

--- EcommerceApi/Models/Category.cs
+++ EcommerceApi/Models/Category.cs
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
-﻿namespace EcommerceApi.Models
-=======
 ﻿using System;
 namespace EcommerceApi.Models
->>>>>>> eed7efd (global config (#1))
 {
 	public class Category
 	{
-		public int Id { get; set; }
 		public string Name { get; set; }
 
-<<<<<<< HEAD
 		public string Image { get; set; }
 
 		public string Description { get; set; }
 
 		public DateTime CreatedAt { get; set; }
-=======
->>>>>>> eed7efd (global config (#1))
 	}
 }
 
