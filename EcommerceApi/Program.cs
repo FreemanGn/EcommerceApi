c8f4bd2fadda7719327379a6be2e1df7cd0f94ee
Freeman_Gn <49251879+FreemanGn@users.noreply.github.com>
2023-10-12 03:22:00 +0100

    Update Program.cs

--- EcommerceApi/Program.cs
+++ EcommerceApi/Program.cs
@@ -22,12 +22,6 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
@@ -35,9 +29,19 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
+    
+}else{
+     builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONNECTION")));
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 
 
